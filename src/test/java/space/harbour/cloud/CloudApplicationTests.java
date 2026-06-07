7477b0d3a0d8edc2f3f9d906e604c4f456b1c813
Igor Sakhankov <igor.sakhankov@glovoapp.com>
2026-06-07 09:34:56 +0200

    feat: Empty spring boot

---
+++ src/test/java/space/harbour/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package space.harbour.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
